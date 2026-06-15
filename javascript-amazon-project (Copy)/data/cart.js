03d86f6be25e27a164e8b6e4f74a4f853be8d5e5
Ademozi <ademoviadem@gmail.com>
2026-06-15 09:53:35 +0100

    adding real dates in the checkout page

--- javascript-amazon-project (Copy)/data/cart.js
+++ javascript-amazon-project (Copy)/data/cart.js
@@ -2,9 +2,11 @@
 export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
 },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
+    deliveryOptionId: '2'
 }];
 
 export function addToCart(productId, quantity) {
@@ -26,7 +28,8 @@ export function addToCart(productId, quantity) {
     // if it is not in the cart, add it to the cart
     cart.push({
         productId, // shorthand property
-        quantity
+        quantity,
+        deliveryOptionId: '1'
     });
     // same as: productId = productId
     // quantity = quantity
